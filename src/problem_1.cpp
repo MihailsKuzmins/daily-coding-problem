c396de1e4137d2b881bbef6b3d483d850a953d09
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-02 21:29:16 +0200

    Problem #1

---
+++ src/problem_1.cpp
@@ -0,0 +1,41 @@
+// Given a list of numbersand a number k, return whether any two numbers from the list add up to k.
+// For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+// Bonus: Can you do this in one pass?
+
+// Example
+// int array[4] = {2, 2, 2, 3};
+// auto size = sizeof(array) / sizeof(*array);
+// auto result = checkHasSum(5, array, size);
+
+#include <vector>
+
+using namespace std;
+
+bool problem_one(int sum, int numbers[], unsigned int size)
+{
+    vector<int> vector;
+
+    for (unsigned int i = 0; i < size; i++)
+    {
+        auto possible_answer = sum - numbers[i];
+        if (possible_answer <= 0)
+            continue;
+
+        auto possible_answer_checked = false;
+        for (auto item : vector)
+        {
+            // already checked
+            if (item == numbers[i])
+            {
+                possible_answer_checked = true;
+                break;
+            }
+            else if (item == possible_answer)
+                return true;
+        }
+
+        if (!possible_answer_checked)
+            vector.push_back(numbers[i]);
+    }
+    return false;
+}
