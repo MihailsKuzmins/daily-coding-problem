07f243ebdc9b867048f00de48dab02c9597b44c7
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-02 21:37:46 +0200

    Problem #2 (#2)
    * Create problem_2.cpp
    
    * Update README.md

---
+++ src/problem_2.cpp
@@ -0,0 +1,50 @@
+// Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+// For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+// Follow-up: what if you can't use division?
+
+// Example:
+// int array[5] = { 1, 2, 3, 4, 5 };
+// auto size = sizeof(array) / sizeof(*array);
+//
+// auto result = problem_two_2(array, size);
+//
+// for (unsigned int i = 0; i < size; i++)
+//     cout << result[i] << endl;
+
+// With division
+int* problem_two_1(int* numbers, unsigned int size)
+{
+	int* new_array = new int[size];
+	int multiplication = 1;
+
+	for (unsigned int i = 0; i < size; i++)
+		multiplication *= numbers[i];
+
+	for (unsigned int i = 0; i < size; i++)
+		new_array[i] = multiplication / numbers[i];
+
+	return new_array;
+}
+
+// Without division
+int* problem_two_2(int* numbers, unsigned int size)
+{
+	int* new_array = new int[size];
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		int multiplication = 1;
+
+		for (unsigned int j = 0; j < size; j++)
+		{
+			if (j == i)
+				continue;
+
+			multiplication *= numbers[j];
+		}
+
+		new_array[i] = multiplication;
+	}
+
+	return new_array;
+}
