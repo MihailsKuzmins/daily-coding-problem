b01fc01b3cc79f6842b3a9c42f02d46c8f90f61f
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-03 20:53:45 +0200

    added the right solution to the second problem

--- src/problem_2.cpp
+++ src/problem_2.cpp
@@ -13,14 +13,37 @@
 //
 // delete array[];
 
-// TODO:
+// Iterating only three times
 int* problem_two_1(int* numbers, unsigned int size)
 {
-	int* new_array = new int[size];
-	return new_array;
+	int* result = new int[size];
+	int* cumulative_left = new int[size];
+	int* cumulative_right = new int[size];
+
+	cumulative_left[0] = 1;
+	cumulative_right[size - 1] = 1;
+
+	for (unsigned int i = 1; i < size; i++)
+		cumulative_left[i] = cumulative_left[i - 1] * numbers[i - 1];
+
+	for (unsigned int i = size - 2;; i--)
+	{
+		cumulative_right[i] = cumulative_right[i + 1] * numbers[i + 1];
+
+		if (i == 0)
+			break;
+	}
+
+	for (unsigned int i = 0; i < size; i++)
+		result[i] = cumulative_left[i] * cumulative_right[i];
+
+	delete[] cumulative_left;
+	delete[] cumulative_right;
+
+	return result;
 }
 
-// Not the besr way
+// Brutal way, not efficient
 int* problem_two_2(int* numbers, unsigned int size)
 {
 	int* new_array = new int[size];
