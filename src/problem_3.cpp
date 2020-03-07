b91ddb8929d4ce24b9ba717d6872b51b62512901
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-07 12:41:17 +0200

    Problem #3, only serialisation (#6)
    * added serialisation
    
    * renamed for_each to for_each_inverse

---
+++ src/problem_3.cpp
@@ -0,0 +1,112 @@
+// Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+// For example, given the following Node class
+//
+// class Node:
+//     def __init__(self, val, left=None, right=None):
+//         self.val = val
+//         self.left = left
+//         self.right = right
+//
+// The following test should pass:
+// node = Node('root', Node('left', Node('left.left')), Node('right'))
+// assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+// Example
+// auto root =
+//     new Node("root",
+//         new Node("left",
+//             new Node("left.left"),
+//             new Node("left.right")),
+//         new Node("right"));
+//
+// NodeSerialiser serialiser(root);
+// std::cout << serialiser.pre_oder();
+
+#include <string>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	const string value;
+	const Node* child_left, *child_right;
+
+	Node(string value, Node* child_left = nullptr, Node* child_right = nullptr): value(value), child_left(child_left), child_right(child_right)
+	{}
+};
+
+class NodeSerialiser
+{
+private:
+	const Node* m_root;
+	void for_each_inverse(const Node* node, void (*f)(const Node* node)) const;
+	string pre_order_internal(const Node* node) const;
+	string in_order_internal(const Node* node) const;
+	string post_order_internal(const Node* node) const;
+public:
+	NodeSerialiser(const Node* root) :m_root(root) {};
+	~NodeSerialiser();
+	string pre_oder() const { return this->pre_order_internal(this->m_root); };
+	string in_order() const { return this->in_order_internal(this->m_root); };
+	string post_order() const { return this->post_order_internal(this->m_root); };
+};
+
+void NodeSerialiser::for_each_inverse(const Node* node, void (*f)(const Node* node)) const
+{
+	if (node == nullptr)
+		return;
+
+	this->for_each_inverse(node->child_left, f);
+	this->for_each_inverse(node->child_right, f);
+	
+	// IMPORTANT for the destructor
+	f(node);
+}
+
+string NodeSerialiser::pre_order_internal(const Node* node) const
+{
+	if (node == nullptr)
+		return "{}";
+
+	return ("{value=\"" + node->value + "\",left=")
+		.append(this->pre_order_internal(node->child_left))
+		.append(",right=")
+		.append(this->pre_order_internal(node->child_right))
+		.append("}");
+}
+
+string NodeSerialiser::in_order_internal(const Node* node) const
+{
+	if (node == nullptr)
+		return "{}";
+
+	return static_cast<string>("{left=")
+		.append(this->in_order_internal(node->child_left))
+		.append(",value=\"" + node->value + "\",right=")
+		.append(this->in_order_internal(node->child_right))
+		.append("}");
+}
+
+string NodeSerialiser::post_order_internal(const Node* node) const
+{
+	if (node == nullptr)
+		return "{}";
+
+	return static_cast<string>("{left=")
+		.append(this->post_order_internal(node->child_left))
+		.append(",right=")
+		.append(this->post_order_internal(node->child_right))
+		.append(",value=\"" + node->value + "\"}");
+}
+
+NodeSerialiser::~NodeSerialiser()
+{
+	auto deleteFunc = [](const Node * node)
+	{
+		delete node;
+	};
+
+	this->for_each_inverse(this->m_root, deleteFunc);
+}
