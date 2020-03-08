353e0d6000c547099bac0994a15ba5dc4933fc51
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-08 17:37:31 +0200

    Two Sum (#11)
    * added a solution for sum_two
    
    * added readme for leet-code examples
    
    * renamed file name
    
    * corrected link to the solution file
    
    * remove bold from code
    
    * updated readme with leet-code

---
+++ src/leet_code/two_sum.cpp
@@ -0,0 +1,33 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+// Example:
+// const int arr[] = { 2,7,11,15 };
+// const int target = 9;
+// const auto size = sizeof(arr) / sizeof(*arr);
+// const auto result = two_sum(target, arr, size);
+
+#include <map>
+
+using namespace std;
+
+auto two_sum(const int target, int const numbers[], const unsigned int size)
+{
+	map<int, unsigned int> map;
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		const auto it = map.find(numbers[i]);
+
+		if (it != map.end())
+			return new unsigned int[2]{ it->second, i };
+		else
+			map.insert({ target - numbers[i], i });
+	}
+
+	throw "Illegal state. Input must have an answer";
+}
