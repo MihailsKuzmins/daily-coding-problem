8c09353654651f9bced85a71280fd49dcc69ec65
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-14 13:19:36 +0200

    Update maximum_subarray.cpp

--- src/leet_code/maximum_subarray.cpp
+++ src/leet_code/maximum_subarray.cpp
@@ -4,14 +4,32 @@
 // Example:
 // int arr[] = { -2,1,-3,4,-1,2,1,-5,4 };
 // auto size = sizeof(arr) / sizeof(*arr);
-// auto result = maximum_subarray(arr, size);
+// auto result = maximum_subarray_1(arr, size);
 
 #include <limits>
 #include <algorithm>
 
 using namespace std;
 
-int maximum_subarray(const int numbers[], const unsigned int size) noexcept
+// Kadane's algorithm
+int max_subarray_1(const int numbers[], const unsigned int size) noexcept
+{
+	int result = numeric_limits<int>::min(), current = 0;
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		current += numbers[i];
+		if (current > result)
+			result = current;
+
+		if (current < 0)
+			current = 0;
+	}
+
+	return result;
+}
+
+int maximum_subarray_2(const int numbers[], const unsigned int size) noexcept
 {
 	int result = numeric_limits<int>::min(), current = result;
 
