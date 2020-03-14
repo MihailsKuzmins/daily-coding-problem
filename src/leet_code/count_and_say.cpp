a1b7862e16223b8bd48295f2eaa3dcbf59dadf94
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-14 12:03:01 +0200

    Count and Say - no recursion (#25)

--- src/leet_code/count_and_say.cpp
+++ src/leet_code/count_and_say.cpp
@@ -2,10 +2,50 @@
 // Note: Each term of the sequence of integers will be represented as a string.
 
 // Example:
-// auto result = count_and_say_2(30);
+// auto result = count_and_say_1(30);
 
 #include <string>
 using namespace std;
+
+// without recursion
+string count_and_say_1(const int n)
+{
+	if (n < 1 || n > 30)
+		throw "Out of range";
+
+	const char base_result = 1;
+
+	switch (n)
+	{
+	case 1: return to_string(base_result);
+	}
+
+	string prev_result = to_string(base_result);
+	string result = "";
+
+	for (auto i = 1; i < n; i++)
+	{
+		for (auto j = 0, current_count = 1; j < prev_result.length(); j++)
+		{
+			if (const int next_index = j + 1; next_index == prev_result.length() || prev_result[j] != prev_result[next_index])
+			{
+				result.append(to_string(current_count)).append(1, prev_result[j]);
+				current_count = 1;
+				continue;
+			}
+
+			current_count++;
+		}
+
+		if (i != n - 1)
+		{
+			prev_result = result;
+			result.clear();
+		}
+	}
+
+	return result;
+}
 
 // recursion
 string count_and_say_2(const int n)
