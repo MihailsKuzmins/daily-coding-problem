883b7391ad21942e2db832db0540efb79dcbaead
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2020-03-18 22:00:50 +0200

    Update binary_tree_level_order_traversal_second.cpp

--- src/leet_code/binary_tree_level_order_traversal_second.cpp
+++ src/leet_code/binary_tree_level_order_traversal_second.cpp
@@ -1,8 +1,26 @@
 // Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
 
+// Example:
+// const auto root = new Node(0,
+//     new Node(2,
+//         new Node(1,
+//             new Node(5),
+//                 new Node(1))),
+//         new Node(4,
+//             new Node(3,
+//                 nullptr,
+//                 new Node(6)),
+//             new Node(-1,
+//                 nullptr,
+//                 new Node(8))));
+//
+// const auto result = binary_tree_level_order_traversal_second(root);
+// delete root;
+
 #include <vector>
-#include <stack>
-#include <iostream>
+#include <queue>
+#include <algorithm>
+
 using namespace std;
 
 struct Node
@@ -14,44 +32,37 @@ struct Node
 	~Node() { delete left; delete right; }
 };
 
-vector<vector<int>> binary_tree_level_order_traversal_second(const Node* const root)
+vector<vector<int>> binary_tree_level_order_traversal_second(const Node* const root) noexcept
 {
 	vector<vector<int>> main_vector;
 
 	if (!root)
 		return main_vector;
 
-	stack<const Node*> stack;
-	const Node* current = root;
+	queue<const Node*> queue;
+	queue.emplace(root);
 
-	while (!stack.empty() || current)
+	while (!queue.empty())
 	{
-		while (current)
-		{
-			cout << current->value << endl;
+		const auto size = queue.size();
+		vector<int> level(size);
 
-			stack.push(current);
-			current = current->left;
+		for (auto i = 0; i < size; i++)
+		{
+			const auto node = queue.front();
+			queue.pop();
+
+			level[i] = node->value;
+
+			if (node->left)
+				queue.emplace(node->left);
+			if (node->right)
+				queue.emplace(node->right);
 		}
 
-		current = stack.top();
-		stack.pop();
-
-		current = current->right;
+		main_vector.emplace_back(move(level));
 	}
 
+	reverse(main_vector.begin(), main_vector.end());
 	return main_vector;
-}
-
-void run_same_tree()
-{
-	const auto root = new Node(3,
-		new Node(9),
-		new Node(20,
-			new Node(15),
-			new Node(7)));
-
-	const auto result = binary_tree_level_order_traversal_second(root);
-
-	delete root;
 }
